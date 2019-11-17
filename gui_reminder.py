906dd851c2600b77cfe4e78d1a72fb4afe0f4690
gego144 <56304474+gego144@users.noreply.github.com>
2019-11-17 16:11:42 -0400

    Update gui_reminder.py

--- gui_reminder.py
+++ gui_reminder.py
@@ -8,31 +8,37 @@ the_date_holder = []
 
 
 def get_text(entry):
-  the_text_holder.append(entry)
+    the_text_holder.append(entry)
+    print(the_text_holder)
 
 
-def get_date(entry):
-    the_date_holder.append(entry)
-    
+def date_limit(entry):
+    if len(entry) != 6 or entry.isdigit() == False:
+        error_label = tk.Label(frame, text="Please make sure you are putting in a number with 6 digits", bg="#696969", font=("arial", 12))
+        error_label.place(relx=0.05, rely=0.54)
+    else:
+        the_date_holder.append(entry)
+        print(the_date_holder)
+
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height = HEIGHT, width = WIDTH)
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 
-frame = tk.Frame(root, bg= "#696969")
-frame.place(relwidth = 1, relheight = 1)
+frame = tk.Frame(root, bg="#696969")
+frame.place(relwidth=1, relheight=1)
 
 
-label = tk.Label(frame,text= "Reminder", bg = "#696969", font = ("arial", 12))
-label.place(relx = 0.05, rely = 0.01)
+label = tk.Label(frame, text="Reminder", bg="#696969", font=("arial", 12))
+label.place(relx=0.05, rely=0.01)
 
-label = tk.Label(frame,text= "Enter reminder:", bg = "#21558c", font = ("arial", 12))
-label.place(relx = 0.05, rely = 0.19)
+label = tk.Label(frame, text="Enter reminder:", bg="#21558c", font=("arial", 12))
+label.place(relx=0.05, rely=0.19)
 
-label = tk.Label(frame,text= "Enter time:", bg = "#21558c", font = ("arial", 12))
-label.place(relx = 0.05, rely = 0.37)
+label = tk.Label(frame, text="Enter time:", bg="#21558c", font=("arial", 12))
+label.place(relx=0.05, rely=0.37)
 
 
 reminder_entry = tk.Entry(frame, bg="white")
@@ -45,8 +51,8 @@ time_entry.place(relx=0.3, rely=0.37, relwidth=0.6)
 button = tk.Button(frame, text="Set text", bg="#8b0000", command=lambda: get_text(reminder_entry.get()))
 button.place(relx=0.91, rely=0.15)
 
-button = tk.Button(frame, text="Set time", bg="#8b0000", command=lambda: get_date(time_entry.get()))
+button = tk.Button(frame, text="Set time", bg="#8b0000", command=lambda: date_limit(time_entry.get()))
 button.place(relx=0.91, rely=0.37)
 
-root.mainloop()
 
+root.mainloop()
