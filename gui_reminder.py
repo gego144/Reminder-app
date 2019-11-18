233ea19b537ed9630c2f396663d7d3bcdddc33c1
gego144 <56304474+gego144@users.noreply.github.com>
2019-11-17 21:24:14 -0400

    Update gui_reminder.py

--- gui_reminder.py
+++ gui_reminder.py
@@ -43,7 +43,7 @@ def open_window():
 def date_limit(entry):
     if len(entry) != 6 or entry.isdigit() == False:
         error_label = tk.Label(frame, text="Please make sure you are putting in a number with 6 digits", bg="#696969", font=("arial", 12))
-        error_label.place(relx=0.05, rely=0.54)
+        error_label.place(relx=0.05, rely=0.72)
     else:
         time_seperated = list(entry)
         # putting it into a list to make divide the values into their respective parts
@@ -88,9 +88,11 @@ label.place(relx=0.05, rely=0.01)
 label = tk.Label(frame, text="Enter reminder:", bg="#21558c", font=("arial", 12))
 label.place(relx=0.05, rely=0.19)
 
-label = tk.Label(frame, text="Enter time:", bg="#21558c", font=("arial", 12))
+label = tk.Label(frame, text="Enter time in format:", bg="#21558c", font=("arial", 12))
 label.place(relx=0.05, rely=0.37)
 
+label = tk.Label(frame, text="Format: hours/minutes/seconds, 00/00/00, without the /", bg="#21558c", font=("arial", 12))
+label.place(relx=0.05, rely=0.54)
 
 reminder_entry = tk.Entry(frame, bg="white")
 reminder_entry.place(relx=0.3, rely=0.19, relwidth=0.6)
